c8094ecdf7f67dfefb569190c9fa1dd57e09c483
LETMIKUK <letmikuk@gmail.com>
2024-10-18 22:47:15 +0700

    feat: admin sidebar (in progress part 2)

--- src/app/components/AdminHeader.tsx
+++ src/app/components/AdminHeader.tsx
@@ -1,9 +1,12 @@
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { AdminSidebar } from "./AdminSidebar";
 
 export default function AdminHeader() {
   return (
-    <header className="bg-white sticky items-center shadow-md w-full top-0 py-4 px-3 flex flex-row justify-between">
-      <span className="font-bold text-2xl">LETMIKUK</span>
+    <header className="bg-white sticky z-[999] items-center w-full top-0 py-4 px-6 flex flex-row justify-between">
+      <div className="flex items-center space-x-3">
+        <span className="font-bold text-2xl">LETMIKUK</span>
+      </div>
       <div className="flex items-center space-x-3">
         <p>Halo, Ryan</p>
         <Avatar>
--- src/app/components/AdminSidebar.tsx
+++ src/app/components/AdminSidebar.tsx
@@ -1,14 +1,79 @@
 "use client";
 
-import { Sheet, SheetContent } from "@/components/ui/sheet";
-import { Layout, Menu } from "antd";
-const { Sider } = Layout;
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/app/components/AdminAccordion";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
-export const AdminSidebar = () => (
-  <Sheet defaultOpen={true}>
-    <SheetContent className="h-full w-[300px]">
-      {" "}
-      <div className="">Test</div>
-    </SheetContent>
-  </Sheet>
-);
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { MenuIcon } from "lucide-react";
+import { useState } from "react";
+
+export const AdminSidebar = () => {
+  const [tab, setTab] = useState("analisa");
+
+  return (
+    <aside className="min-h-full bg-white py-5 px-2">
+      <Accordion
+        className="min-h-full"
+        type="multiple"
+        defaultValue={["analisa", "intervensi", "marketing", "edukasi"]}
+      >
+        <AccordionItem className="border-b-0" value="analisa">
+          <AccordionTrigger
+            className={`${
+              tab === "analisa" ? "bg-sky-100 text-sky-500" : ""
+            } rounded-lg`}
+          >
+            Analisa Stunting per Provinsi
+          </AccordionTrigger>
+          <AccordionContent>
+            <p>Laporan Rangkuman</p>
+            <p>Pengolahan Budget Proyek</p>
+            <p>Pembuatan Proposal Proyek</p>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem className="border-b-0" value="intervensi">
+          <AccordionTrigger>Penanganan Proyek Intervensi</AccordionTrigger>
+          <AccordionContent>
+            <p>Laporan Hasil Proyek</p>
+            <p>Suplai & Tenaga Kerja</p>
+            <p>Pengrekrutan</p>
+          </AccordionContent>
+        </AccordionItem>
+        <Separator className="my-3" />
+        <AccordionItem className="border-b-0" value="marketing">
+          <AccordionTrigger>Penanganan Marketing</AccordionTrigger>
+          <AccordionContent>
+            <p>Laporan Rangkuman</p>
+
+            <p>Whatsapp</p>
+            <p>Instagram</p>
+            <p>Facebook</p>
+            <p>Pengrekrutan</p>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem className="border-b-0" value="edukasi">
+          <AccordionTrigger>Penanganan Edukasi</AccordionTrigger>
+          <AccordionContent>
+            <p>Laporan Rangkuman</p>
+            <p>Instagram</p>
+            <p>Facebook</p>
+            <p>Pengrekrutan</p>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </aside>
+  );
+};
