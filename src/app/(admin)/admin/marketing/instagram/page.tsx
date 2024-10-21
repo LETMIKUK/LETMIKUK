d91a5e84ac0341fa42ce1033dd12e93205535671
nbdy1 <realgamer1525@hotmail.com>
2024-10-21 15:50:51 +0700

    feat(readme): add readme draft

--- src/app/(admin)/admin/marketing/instagram/page.tsx
+++ src/app/(admin)/admin/marketing/instagram/page.tsx
@@ -1,13 +1,13 @@
 "use client";
-import { useToPng } from "@hugocxl/react-to-image";
+import { useToPng, useToSvg } from "@hugocxl/react-to-image";
 import { Button } from "antd";
 
 export default function Page() {
-  const [_, convert, ref] = useToPng<HTMLDivElement>({
+  const [_, convert, ref] = useToSvg<HTMLDivElement>({
     quality: 0.8,
     onSuccess: (data) => {
       const link = document.createElement("a");
-      link.download = "my-image-name.jpeg";
+      link.download = "my-image-name.svg";
       link.href = data;
       link.click();
     },
