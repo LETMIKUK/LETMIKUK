4d38ce861fb8a4529702b8b5bd4e47c3e0ee6c6c
nbdy1 <realgamer1525@hotmail.com>
2024-11-03 23:09:54 +0700

    feat(province_schema): add province schema to sanity

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -2,7 +2,17 @@ import { type SchemaTypeDefinition } from "sanity";
 import dashboardAccount from "./dashboardAccount";
 import appAccount from "./appAccount";
 import augmentImage from "./augmentImage";
+import demografiBalitaProvinsi from "./provinsi/demografiBalitaProvinsi";
+import pengukuranGiziProvinsi from "./provinsi/pengukuranGiziProvinsi";
+import kesiapanPosyanduProvinsi from "./provinsi/kesiapanPosyanduProvinsi";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [dashboardAccount, appAccount, augmentImage],
+  types: [
+    dashboardAccount,
+    appAccount,
+    augmentImage,
+    demografiBalitaProvinsi,
+    pengukuranGiziProvinsi,
+    kesiapanPosyanduProvinsi,
+  ],
 };
