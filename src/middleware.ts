c0fd16e0cf2b2d4d73ca73180a1faf8bc8212095
nbdy1 <realgamer1525@hotmail.com>
2024-11-05 23:55:31 +0700

    fix: middleware for production

--- src/middleware.ts
+++ src/middleware.ts
@@ -39,69 +39,62 @@ export default async function middleware(req: NextRequest) {
     pathname === "/dashboard/login" || pathname === "/dashboard/register";
 
   // Handle /app routes
-  if ((isLocalhost && pathname.startsWith("/app")) || isAppSubdomain) {
-    // If no app cookie, redirect to /app/login
-    if (!appCookie && !isAppAuthPage) {
-      return NextResponse.redirect(new URL("/app/login", req.url));
-    }
 
-    // If already authenticated, redirect from login/register to /app
-    if (appCookie && isAppAuthPage) {
-      return NextResponse.redirect(new URL("/app", req.url));
-    }
-
-    // if (appCookie) {
-    //   try {
-    //     // Verify the JWT token and get the user ID
-    //     const decodedToken = verifyToken(appCookie);
-    //     const userId = decodedToken.userId;
-
-    //     const client = createClient({
-    //       apiVersion:
-    //         process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-10-26",
-    //       dataset: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    //       projectId: process.env.NEXT_PUBLIC_SANITY_DATASET,
-    //       useCdn: true,
-    //       token: process.env.SANITY_TOKEN,
-    //     });
-    //     // Fetch the user's profile completion status from Sanity
-    //     const user = await client.fetch(
-    //       `*[_type == "appAccount" && _id == $id][0]{ isProfileComplete }`,
-    //       { id: userId }
-    //     );
-
-    //     // Redirect to complete-profile if profile is incomplete
-    //     if (
-    //       user &&
-    //       !user.isProfileComplete &&
-    //       pathname !== "/app/complete-profile"
-    //     ) {
-    //       return NextResponse.redirect(
-    //         new URL("/app/complete-profile", req.url)
-    //       );
-    //     }
-    //   } catch (error) {
-    //     console.error("Error in middleware:", error);
-    //     // Redirect to login in case of an error in token verification
-    //     return NextResponse.redirect(new URL("/app/login", req.url));
-    //   }
-    // }
+  // If no app cookie, redirect to /app/login
+  if (!appCookie && !isAppAuthPage) {
+    return NextResponse.redirect(new URL("/app/login", req.url));
   }
 
-  // Handle /dashboard routes
-  if (
-    (isLocalhost && pathname.startsWith("/dashboard")) ||
-    isDashboardSubdomain
-  ) {
-    // If no dashboard cookie, redirect to /dashboard/login
-    if (!dashboardCookie && !isDashboardAuthPage) {
-      return NextResponse.redirect(new URL("/dashboard/login", req.url));
-    }
+  // If already authenticated, redirect from login/register to /app
+  if (appCookie && isAppAuthPage) {
+    return NextResponse.redirect(new URL("/app", req.url));
+  }
 
-    // If already authenticated, redirect from login/register to /dashboard
-    if (dashboardCookie && isDashboardAuthPage) {
-      return NextResponse.redirect(new URL("/dashboard", req.url));
-    }
+  // if (appCookie) {
+  //   try {
+  //     // Verify the JWT token and get the user ID
+  //     const decodedToken = verifyToken(appCookie);
+  //     const userId = decodedToken.userId;
+
+  //     const client = createClient({
+  //       apiVersion:
+  //         process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-10-26",
+  //       dataset: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  //       projectId: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  //       useCdn: true,
+  //       token: process.env.SANITY_TOKEN,
+  //     });
+  //     // Fetch the user's profile completion status from Sanity
+  //     const user = await client.fetch(
+  //       `*[_type == "appAccount" && _id == $id][0]{ isProfileComplete }`,
+  //       { id: userId }
+  //     );
+
+  //     // Redirect to complete-profile if profile is incomplete
+  //     if (
+  //       user &&
+  //       !user.isProfileComplete &&
+  //       pathname !== "/app/complete-profile"
+  //     ) {
+  //       return NextResponse.redirect(
+  //         new URL("/app/complete-profile", req.url)
+  //       );
+  //     }
+  //   } catch (error) {
+  //     console.error("Error in middleware:", error);
+  //     // Redirect to login in case of an error in token verification
+  //     return NextResponse.redirect(new URL("/app/login", req.url));
+  //   }
+  // }
+
+  // If no dashboard cookie, redirect to /dashboard/login
+  if (!dashboardCookie && !isDashboardAuthPage) {
+    return NextResponse.redirect(new URL("/dashboard/login", req.url));
+  }
+
+  // If already authenticated, redirect from login/register to /dashboard
+  if (dashboardCookie && isDashboardAuthPage) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
   // Allow the request if all checks pass
